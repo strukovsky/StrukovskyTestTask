a6cab2ce951639b9fdfdbcbfcd812eb0edd12e6c
dmitry <strukovsky1@gmail.com>
2021-12-14 09:35:57 +0300

    fix: add nullable in main_shop.last_changed

--- main/models.py
+++ main/models.py
@@ -28,7 +28,13 @@ class Shop(models.Model):
     Я поставил в address у параметра on_delete значение CASCADE,
     поскольку решил, что раз удаляется адрес из базы данных,
     то и все соответствующие ему магазины тоже
+
+    Я указал last_changed как NULLABLE, потому что
+    решил, что при создании нового объекта магазина не может быть
+    даты последнего изменения, то есть last_changed
+    В задании не было прописано явно, какое именно поведение использовать:
+    ставить NULL, или дату создания объекта, я решил ставить NULL
     """
     name = models.CharField(max_length=DEFAULT_STRING_LENGTH)
     address = models.ForeignKey('main.Address', on_delete=models.CASCADE, related_name="shops")
-    last_changed = models.DateField()
+    last_changed = models.DateField(null=True)
